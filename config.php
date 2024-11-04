e8d82cd7cdc72fa2c49ef5dafb771da7f639e579
ParreirasJuniorWeb <153013106+ParreirasJuniorWeb@users.noreply.github.com>
2024-11-04 17:16:48 -0300

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
     $dbHost = 'Localhost';
     $dbUsername = 'root';
-    $dbName = 'e_lixo_system';
+    $dbName = '';
 
     $conexao = new mysqli(hostname: $dbHost, username: $dbUsername, password: $dbPassword, database: $dbName);
     $conexao->set_charset(charset: 'utf8mb4');
